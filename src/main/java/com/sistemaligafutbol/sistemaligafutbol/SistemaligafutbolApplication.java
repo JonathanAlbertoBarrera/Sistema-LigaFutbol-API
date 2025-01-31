05186f27a7ecf6e6ce9f07df7191b0d466b9df41
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-01-30 21:09:40 -0600

    creacion del proyecto

---
+++ src/main/java/com/sistemaligafutbol/sistemaligafutbol/SistemaligafutbolApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaligafutbol.sistemaligafutbol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaligafutbolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaligafutbolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemaligafutbol/sistemaligafutbol/SistemaligafutbolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaligafutbol.sistemaligafutbol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaligafutbolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
