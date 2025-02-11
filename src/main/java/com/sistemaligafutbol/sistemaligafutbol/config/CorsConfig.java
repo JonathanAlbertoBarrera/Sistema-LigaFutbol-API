f9cd65832116a1c8725c3394f3124811acc4a37c
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-02-10 19:08:45 -0600

    [➕ add] configuracion del cors para poder hacer consumo de los endpoints

---
+++ src/main/java/com/sistemaligafutbol/sistemaligafutbol/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.sistemaligafutbol.sistemaligafutbol.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica CORS a todos los endpoints
+                        .allowedOrigins("http://localhost:3000","http://localhost:5173","http://localhost:8081") // Solo permite solicitudes desde estos origenes
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Encabezados permitidos
+                        .allowCredentials(true); // Permite el envío de cookies y credenciales
+            }
+        };
+    }
+}
