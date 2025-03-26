0f8b31032171bf9fc82868f92202b81880428d97
JonathanAlbertoBarrera <20233tn110@utez.edu.mx>
2025-03-26 02:46:27 -0600

    peticiones jugador post y update con imagen para movil

---
+++ src/main/java/com/sistemaligafutbol/sistemaligafutbol/modules/jugador/JugadorRegistroRequest.java
@@ -0,0 +1,67 @@
+package com.sistemaligafutbol.sistemaligafutbol.modules.jugador;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+
+import java.time.LocalDate;
+
+public class JugadorRegistroRequest {
+    @NotBlank(message = "Debes de ingresar el nombre completo")
+    private String nombreCompleto;
+
+    @Past(message = "La fecha de nacimiento debe ser anterior a la actual")
+    private LocalDate fechaNacimiento;
+
+    @NotNull(message = "Debes ingresar un número de camiseta")
+    private int numero_camiseta;
+
+    @NotNull(message = "Debes asignar un equipo")
+    private Long idEquipo;
+
+    @NotBlank(message = "La imagen es requerida")
+    private String imagen; // Base64
+
+    // Getters y Setters
+
+    public @NotBlank(message = "Debes de ingresar el nombre completo") String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(@NotBlank(message = "Debes de ingresar el nombre completo") String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public @Past(message = "La fecha de nacimiento debe ser anterior a la actual") LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(@Past(message = "La fecha de nacimiento debe ser anterior a la actual") LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @NotNull(message = "Debes ingresar un número de camiseta")
+    public int getNumero_camiseta() {
+        return numero_camiseta;
+    }
+
+    public void setNumero_camiseta(@NotNull(message = "Debes ingresar un número de camiseta") int numero_camiseta) {
+        this.numero_camiseta = numero_camiseta;
+    }
+
+    public @NotNull(message = "Debes asignar un equipo") Long getIdEquipo() {
+        return idEquipo;
+    }
+
+    public void setIdEquipo(@NotNull(message = "Debes asignar un equipo") Long idEquipo) {
+        this.idEquipo = idEquipo;
+    }
+
+    public @NotBlank(message = "La imagen es requerida") String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(@NotBlank(message = "La imagen es requerida") String imagen) {
+        this.imagen = imagen;
+    }
+}
